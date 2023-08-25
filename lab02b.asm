d3d9bfe6ebb4a69e54b9d0d155b984b26bef5612
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-08-25 20:58:45 -0300

    Create lab02b.asm

---
+++ lab02b.asm
@@ -0,0 +1,13 @@
+#########################################################
+# Laboratório 02 - MCP22105
+# Alocação de dados em memória e Chamada de Sistemas
+#
+# Aluno: Fernando Rodrigues de Santana
+#########################################################
+
+
+#########################################################
+# Faça um programa que imprime a cadeia de caracteres
+# "Hello World!" em linguagem assembler para o MIPS
+#
+#########################################################
