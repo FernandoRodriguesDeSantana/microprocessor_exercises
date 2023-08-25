f871fe2bb22c155590c3b58c26e83d3d2634ab25
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-08-25 15:18:04 -0300

    Update lab02.asm

--- lab02.asm
+++ lab02.asm
@@ -4,3 +4,24 @@
 #
 # Aluno: Fernando Rodrigues de Santana
 #########################################################
+
+#########################################################
+# Considere o seguinte programa em linguagem 
+# Assembly do MIPS
+#
+#    .data 0x10010400 # segmento de dados
+#      palavra1: .word 13
+#      palavra2: .word 0x15
+#
+# Indique, em hexadecimal, os endereços de memória dos 
+# símbolos palavra1 e palavra2
+
+
+# RESPOSTA: 
+# Uma palavra (.word) ocupa 4 bytes (0x4) na memória.
+
+# Dessa forma, o endereço de palavra1 será: 0x10010400,
+# ocupando os primeiros 4 bytes deste segmento de endereço base
+
+ # Portanto, o endereço de palavra2 será: 0x10010404
+##########################################################
