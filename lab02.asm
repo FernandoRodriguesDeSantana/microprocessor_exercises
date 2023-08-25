0128aafedaca7459b3979025da8a70b85cb3cc31
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-08-25 15:28:16 -0300

    Update lab02.asm

--- lab02.asm
+++ lab02.asm
@@ -24,3 +24,28 @@
 
  # Portanto, o endereço de palavra2 será: 0x10010404
 ##########################################################
+
+#########################################################
+# Considere o seguinte programa em linguagem 
+# Assembly do MIPS
+#
+#    .data 0x10010800 # segmento de dados
+#
+#      variavel_a: .word 13
+#      nums:       .word 2, 6, 8, 5, 98, 74, 28
+#
+# Indique, em hexadecimal, o endereço do elemento com o
+# valor 74 do vetor nums
+
+
+# RESPOSTA:
+
+# adress(variavel_a) = 0x10010800
+# adress(nums[0]) = 0x10010804
+# adress(nums[1]) = 0x10010808
+# adress(nums[2]) = 0x1001040C
+# adress(nums[3]) = 0x10010410
+# adress(nums[4]) = 0x10010414
+# adress(nums[5]) = 0x10010418 -> endereço do elemento com o valor 72
+# adress(nums[6]) = 0x1001041C
+##########################################################
