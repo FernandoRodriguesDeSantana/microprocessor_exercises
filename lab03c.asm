d9591e459acb3589d45aef81b4469b8589f72d2a
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-09-03 17:06:49 -0300

    Create lab03c.asm

---
+++ lab03c.asm
@@ -0,0 +1,30 @@
+#########################################################
+# Laboratório 03 - MCP22105
+# Estruturas de Controle
+#
+# Aluno: Fernando Rodrigues de Santana
+#########################################################
+
+#########################################################
+# Faça um programa para buscar um determinado valor em um 
+# array de inteiros. O endereço inicial do vetor está 
+# armazenado no endereço de memória 0x10010000, o tamanho 
+# do vetor está no endereço 0x10010004 e valor que será 
+# pesquisado está no endereço 0x10010008. Caso o valor 
+# seja encontrado, escreva 0x01 no endereço 0x1001000C, 
+# caso contrário, escreva 0x00.
+#
+#########################################################
+
+
+#########################################################
+# Faça um programa para contar o número de elementos 
+# encontrados em um array de inteiros. O endereço inicial 
+# do vetor está armazenado no endereço de memória 0x10010000, 
+# o tamanho do vetor está no endereço 0x10010004 e valor que 
+# será contado está no endereço 0x10010008. Armazene no 
+# endereço 0x1001000C o número de elementos encontrados 
+# na procura.
+#
+#########################################################
+
