1dfc05060faa5db7781e8919b80a720da17feca9
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-09-03 14:32:07 -0300

    Create lab03b.asm

---
+++ lab03b.asm
@@ -0,0 +1,42 @@
+#########################################################
+# Laboratório 03 - MCP22105
+# Estruturas de Controle
+#
+# Aluno: Fernando Rodrigues de Santana
+#########################################################
+
+#########################################################
+# Faça um programa que leia o conteúdo da posição de 
+# memória 0x10010000 e armazene-a na posição 0x10010004 
+# se ela for positiva, ou 0x10010008 se for negativa.
+#
+#########################################################
+
+
+
+#########################################################
+# Faça um programa que teste se o conteúdo da posição de 
+# memória 0x10010000 e 0x10010004 são iguais e, em caso 
+# positivo, armazene o valor na posição 0x10010008.
+#
+#########################################################
+
+
+#########################################################
+# Faça um programa que leia o conteúdo da posição de 
+# memória 0x10010000 e 0x10010004 e, armazene o maior 
+# deles na posição 0x10010008.
+#
+#########################################################
+
+
+#########################################################
+# Faça um programa que leia 3 notas dos endereços 
+# 0x10010000, 0x10010004 e 0x10010008 e, sabendo que a 
+# média é 7, armazene 1 no endereço 0x1001000C caso ele 
+# esteja aprovado ou no endereço 0x10010010 caso ele 
+# esteja reprovado.
+#
+#########################################################
+
+
