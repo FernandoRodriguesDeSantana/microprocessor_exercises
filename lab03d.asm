e4324d6ca0f14bcd228080fa23e645f4e759b76b
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-09-06 10:49:15 -0300

    Update lab03d.asm

--- lab03d.asm
+++ lab03d.asm
@@ -25,5 +25,14 @@
 .text
 	la $t0, Word		# carregando em $t0 o endereço da string "Word"
 	sw $t0, Adress		# carregando este endereço em 0x10010000
+	
+
+loop:	lb $t1, 0($t0)		# carrega o byte atual da string em $t1	
+	beq $t1, 0, then	# compara o byte em $t1 com 0 (NULL)
+	add $t0, $t0, 1		# adiciona 1 ao endereço para procurar o próximo byte
+	add $t2, $t2, 1		# adiciona 1 ao $t2 cuja função é a de contar o numero de caracteres
+	j   loop
+then:	
+	sw $t2, Result		# armazena o valor de $t3 no adress 0x10010004
 
 #########################################################
