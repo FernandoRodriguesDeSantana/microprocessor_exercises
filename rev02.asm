2dc9af3c7b1f82bedd63af01d029b57f8403be87
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-09-19 21:52:46 -0300

    Update rev02.asm

--- rev02.asm
+++ rev02.asm
@@ -19,13 +19,21 @@
 #    }
 # }
 #########################################################
-li $s0, 3 # a
-li $s1, 4 # b
-li $s2, 2 # c
+li $s0, 4 # a
+li $s1, 3 # b
+li $s2, 1 # c
 li $s3, 5 # d
 li $s4, 3 # i
 
-	bne $s0, $s0, then1:
-
+	bne  $s0, $s1, then1 # if(a != b)
+	
+loop:	add  $s1, $s1, $s4 # b = b + i
+ 	addi $s4, $s4, 2 # i = i + 2
+ 	blt  $s4, 15, loop # for(int i = 3; i < 15; i += 2)
+ 	j    end
 then1: 
- 	  
+	la   $s0, ($s1)	# a = b
+	
+	blt  $s2, 3, then2 # if(c < 3)
+then2:  addi $s0, $s0, 1 # a = a + i
+end:
