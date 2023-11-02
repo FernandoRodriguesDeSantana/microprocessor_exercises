ced5d8544e38aea49dc5d77258c3f7f0ccba9001
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-11-02 11:27:12 -0300

    Update lab04_ex02.asm

--- lab04_ex02.asm
+++ lab04_ex02.asm
@@ -42,21 +42,21 @@ str1:       .asciiz "MCP22105 is cool"
 
 .text
 	print_str("Str1: ")
-	print_str_ptr(str1)	#PONTEIRO EM 0x10010011 ("Str1:")
+	print_str_ptr(str1)		#PONTEIRO EM 0x10010011 ("Str1:")
 	print_str("\n")
 	
 	## Chamar strlen
-	la $a0, str1		#CARREGA O ENDEREÇO 0X10010000 (FRASE) EM $a0 
-	jal strlen		#JUMP AND LINK PARA A FUNÇÃO STRLEN
-	move $s0, $v0		#s0 RECEBE O CONTEÚDO DE $v0 (APARENTEMENTE QTD DE LETRAS)
+	la $a0, str1			#CARREGA O ENDEREÇO 0X10010000 (FRASE) EM $a0 
+	jal strlen			#JUMP AND LINK PARA A FUNÇÃO STRLEN
+	move $s0, $v0			#s0 RECEBE O CONTEÚDO DE $v0 (QTD DE LETRAS)
 	
 	print_str("A str1 tem ")
-	print_int_reg($s0)
+	print_int_reg($s0)		#EXIBE NO PROMPT A QTD DE LETRAS
 	print_str(" caracteres\n")
 	
 	## Resize str1
-	la $a0, str1
-	li $a1, 8
+	la $a0, str1			#CARREGA O ENDEREÇO DA FRASE STR1 EM $a0
+	li $a1, 8			#PARÂMETRO RESIZE (QTD DE LETRAS)DA FUNÇÃO STRRESIZE
 	jal strResize
 	
 	print_str("Str1 ajustada: ")
@@ -89,7 +89,8 @@ str1:       .asciiz "MCP22105 is cool"
 # size.
 #
 strResize:
-
+#$a0 POSSUI O ENDEREÇO DA FRASE
+	#lb $t1, 0($a0)		#CARREGA A LETRA ARMAZENADA NO ENDEREÇO )0x100100XX
 	jr $ra
 #############################################
 
