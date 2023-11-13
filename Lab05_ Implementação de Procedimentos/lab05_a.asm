1bd5cbfa6e0acb9047358e96d6d6887f835c1d9c
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-11-13 10:37:21 -0300

    Update lab05_a.asm

--- Lab05_ Implementação de Procedimentos/lab05_a.asm
+++ Lab05_ Implementação de Procedimentos/lab05_a.asm
@@ -51,7 +51,7 @@ main:
 
 	li $s0, 11 #
 main_L0:
-	beqz $s0, main_L0_end #INICIO
+	beqz $s0, main_L0_end
 	addi $s0, $s0, -1
 	
 	print_str("fatorial(")
@@ -60,12 +60,12 @@ main_L0:
 	
 	move $a0, $s0
 	jal  fatorial
-	move $s1, $v0	#MEIO
+	move $s1, $v0
 	
 	print_intReg($s1)
 	print_str("\n")
 	
-	j main_L0	#FIM
+	j main_L0
 main_L0_end:
   lw	$ra, 12($sp)
   lw    $s1, 8($sp)
@@ -74,7 +74,24 @@ main_L0_end:
 	jr $ra
 ############################################
 fatorial:
-	jr $ra
+	addi $sp, $sp, -8	#aloca��o din�mica de mem�ria
+	sw   $ra, 4($sp)	#endere�o de $ra armazenado na 2� posi��o de $sp
+	sw   $a0, 0($sp)	#argumento n armazenado na 1� posi��o de $sp
+	
+	bnez $a0, else		
+	addi $v0, $zero, 1	#$v0 retorna 1
+	addi $sp, $sp, 8	#desaloca��o de mem�ria
+	jr   $ra		#volta para o endere�o armazenado em $ra
+else:
+	addi $a0, $a0, -1	#itera��o do argumento
+ 	jal  fatorial		#la�o de repeti��o
+ 
+	lw   $a0, 0 ($sp)	#argumento n armazenado na 1� posi��o de $sp
+	lw   $ra, 4 ($sp)	#endere�o de $ra armazenado na 2� posi��o de $sp
+	addi $sp, $sp, 8 	#desaloca��o de mem�ria
+	mul  $v0, $a0, $v0	#multiplica��o fatorial
+	jr   $ra
+	
 ######################
 # unsigned int fatorial(unsigned int n){ 
 #     unsigned int ret;
