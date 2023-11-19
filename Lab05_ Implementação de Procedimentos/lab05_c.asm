ccb3e708a2b32867f3cdb38d771b297e6d25161a
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-11-19 00:26:27 -0300

    Update lab05_c.asm

--- Lab05_ Implementação de Procedimentos/lab05_c.asm
+++ Lab05_ Implementação de Procedimentos/lab05_c.asm
@@ -136,3 +136,111 @@ bubble:
 #             v[j+1] = aux;
 	add  $t4, $t5, $zero
 	jr $ra
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################
+# Laboratório 03 - MCP22105
+# Procedimentos
+#
+# Aluno: Fernando Rodrigues de Santana
+#########################################################
+.include "macros.inc"
+.text 0x00400000
+init:
+	la $sp, 0x7FFFEFFC
+	jal main
+	exit
+.include "string.inc"
+
+.data
+vetor:  .word 45,3,7,89,32,76
+
+.data 0x10010020
+vetorB: .word 78,2,6,90,124,76,34,71
+
+.text
+
+#-----------
+# ($a0) old_stk -> 16($sp)
+#-----------
+# <empty>      -> 12($sp)
+#-----------
+#   $ra        -> 8($sp)
+#-----------
+#   $a1        -> 4($sp)
+#-----------
+#   $a0        -> 0($sp)
+#############################################
+main:
+	addiu $sp, $sp, -16
+	sw    $ra, 8($sp)
+
+	la  $a0, vetor
+	li  $a1, 6	
+	jal bubble
+	
+	la  $a0, vetorB
+	li  $a1, 8	
+	jal bubble
+
+	lw    $ra, 8($sp)
+	addiu $sp, $sp, 16
+	jr $ra
+
+#########################################################
+# void bubble(int* v, int size) {
+#	int i; # t0
+#	int j; # t1
+#	int aux;
+#	int k = size - 1 ; # t2
+#
+#   for(i = 0; i < size; i++) {
+#      for(j = 0; j < k; j++) {
+#         if(v[j] > v[j+1]) {
+#             aux = v[j];
+#             v[j] = v[j+1];
+#             v[j+1] = aux;
+#         }
+#      }
+#      k--;
+#   }
+# }
+########################################################
+bubble:
+	addiu $sp,   $sp, -16
+	sw    $a0,  0($sp)
+	sw    $a1,  4($sp)
+	sw    $ra,  8($sp)
+
+#	int i; # t0
+	li   $t0, 0
+	
+#	int j; # t1
+	li   $t1, 0
+
+#	int k = size - 1 ; # t2
+	addi $t2, $a1, -1  
+	
+#   	for(i = 0; i < size; i++) {	
+	bge  $t0, $a1, end
+	addi $t0, $t0, 1   #i++
+	
+#       for(j = 0; j < k; j++) {
+	bge  $t1, $t2, end
+	addi $t1, $t1, 1   #j++ 	
+
+#       if(v[j] > v[j+1]) {
+	
+end:
+	jr $ra
